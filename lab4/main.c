d1d5c4988edc241965ecf58898e6102ebc67d2d2
miiva-prog <ivan.mikhalov.1104@gmail.com>
2024-03-18 20:35:36 +0700

    lab 4

---
+++ lab4/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define HEIGHT 4.5
+
+typedef struct 
+{
+    double x;
+    double y;
+}Point;
+
+double interpolation(int n,double px,Point p[n]);
+
+int main()
+{
+    FILE *data = fopen("data.txt","w");
+    FILE *point = fopen("point.txt","w");
+    int size = 4;
+    double interpol;
+    Point p[size];
+
+    for(int n = 0;n < size;n++){
+        printf("x%d ->",n);
+        scanf("%lf",&p[n].x);
+        printf("y%d ->",n);
+        scanf("%lf",&p[n].y);
+    }
+
+    for(int n = 0;n < size;n++)
+        fprintf(data,"%f     %f\n",p[n].x,p[n].y);
+
+    printf("interpolation ->");
+    scanf("%lf",&interpol);
+
+    double P_inter = interpolation(size,interpol,p);
+
+    fprintf(point,"%f     %f\n",P_inter,HEIGHT);
+    fclose(data);
+    fclose(point);
+
+    return 0;
+}
+
+double interpolation(int n,double px,Point p[n])
+{
+    double P_inter = 0.0;
+    double value;
+
+    for(int i = 0;i < n;i++){
+        value = 1.0;
+        for(int j = 0;j < n;j++){
+            if(i != j)
+                value *= (px - p[j].x) / (p[i].x - p[j].x);
+        }
+        P_inter += p[i].y * value;
+    }
+
+    return P_inter;
+}
+
+/*plot 'data.txt' title "X Y" with linespoints,\
+'point.txt' title "interpolation" with points*/
